254009154e04dc1ec6952b9c102ba4402877ae06
Cahrishma sri <charishmasri06@gmail.com>
2026-05-09 11:33:35 +0530

    fix maven wrapper version

--- ngo-project-main/ngo-connect-backend/src/main/java/com/ngoconnect/NgoConnectBackendApplication.java
+++ ngo-project-main/ngo-connect-backend/src/main/java/com/ngoconnect/NgoConnectBackendApplication.java
@@ -2,14 +2,17 @@ package com.ngoconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ngoconnect"})
+@ComponentScan(basePackages = { "com.ngoconnect" })
+@EnableJpaRepositories(basePackages = { "com.ngoconnect.repository" })
+@EntityScan(basePackages = { "com.ngoconnect.entity" })
 public class NgoConnectBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NgoConnectBackendApplication.class, args);
     }
-
 }
